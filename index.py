ec8e6cba32546b90277bce9b68c882f93e91a5df
Ramana Sonti <sontivr@yahoo.com>
2018-06-27 20:32:29 +0000

    Commit 2

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
 import json
 import datetime
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    logger.info('got event{}'.format(event))
+    logger.error('something went wrong')
+
     data = {
         'output': 'Hello World - from Cloud9!',
         'timestamp': datetime.datetime.utcnow().isoformat()
@@ -10,3 +16,6 @@ def handler(event, context):
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+            
+            
+
