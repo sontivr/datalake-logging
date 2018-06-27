4178314291b73783c71114f5c3c9dce564278d4b
Ramana Sonti <sontivr@yahoo.com>
2018-06-27 19:31:57 +0000

    Initial commit

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World - from Cloud9!',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
